e2fb3b6a5c78b412775b73511b34274133b5b619
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2021-07-26 15:56:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
+git clone https://github.com/small-5/luci-app-adblock-plus package/adblock-plus
