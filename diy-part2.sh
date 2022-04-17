5e58663780da153a25fb8a7bc4f71ea1c06efd5a
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2022-04-17 11:41:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,5 +18,10 @@
 #rm -rf package/helloworld
 #git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 cd package/lean/
+rm -rf lua-maxminddb
 git clone https://github.com/jerrykuku/lua-maxminddb.git
+rm -rf luci-app-vssr
 git clone https://github.com/jerrykuku/luci-app-vssr.git
+rm -rf luci-theme-neobird
+git clone https://github.com/lwb1978/luci-theme-neobird.git
+cd ~/openwrt/
