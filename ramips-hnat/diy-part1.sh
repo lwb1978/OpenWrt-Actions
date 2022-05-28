7d9e4a17ff5331ba88b7dc10b4f5016340dbddf3
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2022-05-28 10:59:45 +0800

    Update diy-part1.sh

--- ramips-hnat/diy-part1.sh
+++ ramips-hnat/diy-part1.sh
@@ -11,10 +11,10 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-rm -rf target/linux/ramips
-svn export https://github.com/padavanonly/immortalwrt/trunk/target/linux/ramips target/linux/ramips
+# rm -rf target/linux/ramips
+# svn export https://github.com/padavanonly/immortalwrt/trunk/target/linux/ramips target/linux/ramips
