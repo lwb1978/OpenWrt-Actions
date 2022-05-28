40dcbf615431551143f946b64c5b218f1b272d44
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2022-05-28 11:17:11 +0800

    Update diy-part2.sh

--- ramips-hnat/diy-part2.sh
+++ ramips-hnat/diy-part2.sh
@@ -23,7 +23,7 @@ git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 rm -rf package/passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
 
-cd package/lean/
+cd package/emortal/
 rm -rf lua-maxminddb
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 rm -rf luci-app-vssr
