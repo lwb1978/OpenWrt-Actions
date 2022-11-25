ed9c643ea92279bf73c6dfe3dbf469c4a093b1f6
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2022-11-25 15:44:19 +0800

    Update diy-part1.sh

--- arm_x86/diy-part1.sh
+++ arm_x86/diy-part1.sh
@@ -11,7 +11,12 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# Add a feed source 1
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# add a feed source 2
+# echo 'src-git lienol https://github.com/Lienol/openwrt-package' >>feeds.conf.default
+
+# 设置默认主题
+# sed -i ' s/luci-theme-bootstrap/luci-theme-neobird/g ' feeds/luci/collections/luci/Makefile
