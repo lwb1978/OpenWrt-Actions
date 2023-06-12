be001c5065e97adc8c0cae1fbb9a1a35d2e3e7ee
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2023-06-12 13:58:13 +0800

    Update diy-part2.sh

--- arm_x86/diy-part2.sh
+++ arm_x86/diy-part2.sh
@@ -29,28 +29,22 @@ sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=65535' package
 # git clone https://github.com/small-5/luci-app-adblock-plus package/adblock-plus
 
 # SSR 翻墙
-rm -rf feeds/luci/applications/luci-app-ssrp
-git clone --depth=1 https://github.com/fw876/helloworld.git feeds/luci/applications/luci-app-ssrp
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
 # passwall 翻墙
-# rm -rf feeds/luci/applications/passwall_packages
-# git clone https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/passwall_packages
-# rm -rf package/applications/luci-app-passwall
-# git clone -b luci https://github.com/xiaorouji/openwrt-passwall feeds/luci/applications/luci-app-passwall
+# rm -rf package/passwall_packages
+# git clone https://github.com/xiaorouji/openwrt-passwall package/passwall_packages
+# rm -rf package/passwall_luci
+# git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall_luci
 
 # 添加 smartdns
-rm -rf feeds/luci/applications/luci-app-smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+rm -rf package/smartdns_luci
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/smartdns_luci
 # 更新lean的内置的smartdns版本
 # sed -i 's/1.2021.35/2022.03.02/g' feeds/packages/net/smartdns/Makefile
 # sed -i 's/f50e4dd0813da9300580f7188e44ed72a27ae79c/1fd18601e7d8ac88e8557682be7de3dc56e69105/g' feeds/packages/net/smartdns/Makefile
 # sed -i 's/^PKG_MIRROR_HASH/#&/' feeds/packages/net/smartdns/Makefile
-
-# 添加主题
-# rm -rf feeds/luci/themes/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-neobird
-git clone https://github.com/lwb1978/luci-theme-neobird.git feeds/luci/themes/luci-theme-neobird
 
 pushd package/lean/
 # helloworld 翻墙
@@ -58,7 +52,11 @@ pushd package/lean/
 # git clone https://github.com/jerrykuku/lua-maxminddb.git
 # rm -rf luci-app-vssr
 # git clone https://github.com/jerrykuku/luci-app-vssr.git
-
+# 添加主题
+# rm -rf luci-theme-argon
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+rm -rf luci-theme-neobird
+git clone https://github.com/lwb1978/luci-theme-neobird.git
 # rm -rf luci-app-omcproxy
 # git clone -b 18.06 https://github.com/lwb1978/luci-app-omcproxy.git
 popd
