74be3875c4b46ac24a43330afa50fa021c8130e7
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2023-08-18 09:07:16 +0800

    Update diy-part2.sh

--- arm_x86/diy-part2.sh
+++ arm_x86/diy-part2.sh
@@ -40,8 +40,9 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/passwall_package
 git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall package/passwall_luci
 
 # 添加 smartdns
-rm -rf package/applications/luci-app-smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/applications/luci-app-smartdns
+# rm -rf package/applications/luci-app-smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/pymumu/smartdns.git package/smartdns
 # 更新lean的内置的smartdns版本
 # sed -i 's/1.2021.35/2022.03.02/g' feeds/packages/net/smartdns/Makefile
 # sed -i 's/f50e4dd0813da9300580f7188e44ed72a27ae79c/1fd18601e7d8ac88e8557682be7de3dc56e69105/g' feeds/packages/net/smartdns/Makefile
