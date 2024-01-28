7699b412ca7298261c82ac923e507ce57e4c8fd3
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2024-01-28 22:56:51 +0800

    Update diy-part2.sh

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -10,6 +10,34 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+# 拉取仓库文件夹
+function merge_package() {
+	# 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+	# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+	if [[ $# -lt 3 ]]; then
+	echo "Syntax error: [$#] [$*]" >&2
+	return 1
+	fi
+	trap 'rm -rf "$tmpdir"' EXIT
+	branch="$1" curl="$2" target_dir="$3" && shift 3
+	rootdir="$PWD"
+	localdir="$target_dir"
+	[ -d "$localdir" ] || mkdir -p "$localdir"
+	tmpdir="$(mktemp -d)" || exit 1
+	git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+	cd "$tmpdir"
+	git sparse-checkout init --cone
+	git sparse-checkout set "$@"
+	# 使用循环逐个移动文件夹
+	for folder in "$@"; do
+	mv -f "$folder" "$rootdir/$localdir"
+	done
+	cd "$rootdir"
+}
+# 示例:
+# merge_package master https://github.com/WYC-2020/openwrt-packages package/openwrt-packages luci-app-eqos luci-app-openclash luci-app-ddnsto ddnsto 
+# merge_package master https://github.com/lisaac/luci-app-dockerman package/lean applications/luci-app-dockerman
+
 # 默认IP由1.1修改为0.1
 # sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
@@ -140,32 +168,3 @@ fi
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
-# 拉取仓库文件夹
-function merge_package() {
-	# 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
-	# 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
-	if [[ $# -lt 3 ]]; then
-	echo "Syntax error: [$#] [$*]" >&2
-	return 1
-	fi
-	trap 'rm -rf "$tmpdir"' EXIT
-	branch="$1" curl="$2" target_dir="$3" && shift 3
-	rootdir="$PWD"
-	localdir="$target_dir"
-	[ -d "$localdir" ] || mkdir -p "$localdir"
-	tmpdir="$(mktemp -d)" || exit 1
-	git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
-	cd "$tmpdir"
-	git sparse-checkout init --cone
-	git sparse-checkout set "$@"
-	# 使用循环逐个移动文件夹
-	for folder in "$@"; do
-	mv -f "$folder" "$rootdir/$localdir"
-	done
-	cd "$rootdir"
-}
-
-# 示例:
-# merge_package master https://github.com/WYC-2020/openwrt-packages package/openwrt-packages luci-app-eqos luci-app-openclash luci-app-ddnsto ddnsto 
-# merge_package master https://github.com/lisaac/luci-app-dockerman package/lean applications/luci-app-dockerman
