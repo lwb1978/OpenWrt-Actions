2e4976b6b5d56754f2b3a335b026e175ff382643
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2024-04-02 15:31:01 +0800

    内核更新到6.6

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -145,8 +145,8 @@ git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git p
 # default_theme='Argon'
 # sed -i "s/bootstrap/$default_theme/g" feeds/luci/modules/luci-base/root/etc/config/luci
 
-# 修改Rockchip内核到6.1版
-sed -i 's/5.15/6.1/g' ./target/linux/rockchip/Makefile
+# 修改Rockchip内核到6.6版
+sed -i 's/6.1/6.6/g' ./target/linux/rockchip/Makefile
 # 修改x86内核到6.6版
 sed -i 's/6.1/6.6/g' ./target/linux/x86/Makefile
 
