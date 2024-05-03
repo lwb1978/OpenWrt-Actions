dc0d43d82020b8ab2673adf5b20524f708408186
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2024-05-04 02:07:16 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -125,6 +125,10 @@ pushd package/libs/openssl/patches
     curl -sO https://$mirror/openwrt/patch/openssl/quic/0044-QUIC-Update-metadata-version.patch
 popd
 
+# openssl 3.2
+# rm -rf package/libs/openssl
+# git clone https://github.com/sbwml/package_libs_openssl -b openssl-3.2 package/libs/openssl
+	  
 # openssl -Ofast
 sed -i "s/-O3/-Ofast/g" package/libs/openssl/Makefile
 
