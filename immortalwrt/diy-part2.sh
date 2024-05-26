d547390aa313c7b6d73ff4709831b3e3f31c7575
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2024-05-26 18:32:16 +0800

    Update diy-part2.sh

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -43,10 +43,8 @@ merge_package v5 https://github.com/sbwml/openwrt_helloworld package/passwall-pa
 rm -rf feeds/luci/applications/{luci-app-passwall,luci-app-ssr-libev-server}
 git clone -b luci-smartdns-dev --single-branch https://github.com/lwb1978/openwrt-passwall package/passwall-luci
 # git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
-# 修改：延时启动时先创建smartdns配置文件，以免smartdns启动失败
-pushd package/passwall-luci
-	patch -p1 ${GITHUB_WORKSPACE}/patch/passwall/01_boot_touch_smartdns_conf.patch
-popd
+# 延时启动时先创建smartdns配置文件，以免smartdns启动失败
+cp -rf ${GITHUB_WORKSPACE}/patch/passwall/root/etc/init.d/passwall package/passwall-luci/luci-app-passwall/root/etc/init.d/
 # ------------------------------------------------------------
 
 # 优化socat中英翻译
