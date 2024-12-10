b26bc426750bef5516f9d0dc65ff4d879125d5d2
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2024-12-10 21:41:02 +0800

    调整分区大小

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -82,8 +82,8 @@ merge_package openwrt-24.10 https://github.com/sbwml/luci-theme-argon feeds/luci
 #git clone https://github.com/sbwml/feeds_packages_utils_unzip feeds/packages/utils/unzip
 
 # golang 1.23
-#rm -rf feeds/packages/lang/golang
-#git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 
 # ppp - 2.5.1
 #rm -rf package/network/services/ppp
