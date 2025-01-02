726433b36bf24a380c7f16857cd5aac39cf2477c
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2025-01-02 17:07:59 +0800

    测试集客AC

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -66,6 +66,7 @@ git clone https://github.com/sirpdboy/luci-app-lucky package/lucky-packages
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky-packages
 
 # 集客AC控制器
+git clone https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 # git clone -b v1.0 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 
 # 添加主题
