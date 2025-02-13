812e13dbb88c8615b9b746bb3262d20bd7b6fcee
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2025-02-13 11:32:03 +0800

    curl: update to 8.12.0

--- lede/diy-part2.sh
+++ lede/diy-part2.sh
@@ -83,7 +83,7 @@ sed -i 's#_(\"udpxy\")#_(\"UDPXY\")#g' feeds/luci/applications/luci-app-udpxy/lu
 
 # 替换curl修改版（无nghttp3、ngtcp2）
 curl_ver=$(cat feeds/packages/net/curl/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}')
-[ "$(check_ver "$curl_ver" "8.11.1")" != "0" ] && {
+[ "$(check_ver "$curl_ver" "8.12.0")" != "0" ] && {
 	echo "替换curl版本"
 	rm -rf feeds/packages/net/curl
 	cp -rf ${GITHUB_WORKSPACE}/patch/curl feeds/packages/net/curl
