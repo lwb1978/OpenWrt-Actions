382f9b36280de9c82bcb116d41813e1895091c8f
A-BO <86697442+lwb1978@users.noreply.github.com>
2025-06-12 21:46:06 +0800

    openssl enable QUIC

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -60,7 +60,9 @@ rm -rf feeds/packages/net/smartdns
 cp -rf ${GITHUB_WORKSPACE}/patch/smartdns package/
 
 # openssl Enable QUIC and KTLS support
-curl -s https://github.com/openwrt/openwrt/commit/7a01d41a376a716179de12203b1e255a5618b4fa.patch | patch -p1
+curl -s https://github.com/openwrt/openwrt/commit/c2e31f42ee0bc2ed8024afe99685c4cdf2677bf4.patch | patch -p1
+echo "CONFIG_OPENSSL_WITH_QUIC=y" >> .config
+# echo "CONFIG_OPENSSL_WITH_KTLS=y" >> .config
 
 # 替换udpxy为修改版，解决组播源数据有重复数据包导致的花屏和马赛克问题
 rm -f feeds/packages/net/udpxy/Makefile
