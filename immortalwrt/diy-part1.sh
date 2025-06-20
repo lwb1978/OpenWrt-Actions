3fc8042ad67dd6f3d5fe2e0828a74253103c1ebb
lwb1978 <86697442+lwb1978@users.noreply.github.com>
2025-06-20 11:19:20 +0800

    optimize

--- immortalwrt/diy-part1.sh
+++ immortalwrt/diy-part1.sh
@@ -13,6 +13,9 @@
 echo "开始 DIY1 配置……"
 echo "========================="
 
+chmod +x ${GITHUB_WORKSPACE}/immortalwrt/function.sh
+source ${GITHUB_WORKSPACE}/immortalwrt/function.sh
+
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
@@ -23,7 +26,13 @@ echo "========================="
 # svn export https://github.com/padavanonly/immortalwrt/trunk/target/linux/ramips target/linux/ramips
 
 # openssl Enable QUIC and KTLS support
-curl -s https://github.com/openwrt/openwrt/commit/362aea4649485ca7c31ce42c371d5051e7dead4d.patch | patch -p1
+openssl_ver=$(cat package/libs/openssl/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}')
+[ "$(check_ver "$openssl_ver" "3.5.0")" != "0" ] && {
+	curl -s https://github.com/openwrt/openwrt/commit/362aea4649485ca7c31ce42c371d5051e7dead4d.patch | patch -p1
+	pushd package/libs/openssl/patches
+	curl -sSL https://github.com/openssl/openssl/commit/99ea6b38430dc977ba63c832694cdb3c2cb3c2c9.patch -o 900-Add-NULL-check-in-ossl_quic_get_peer_token.patch
+	popd
+}
 
 # 修改系统版本（界面显示）
 VERSION=${GITHUB_WORKSPACE}/immortalwrt/version
