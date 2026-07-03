bee6b93075b756c50109190da74ca5e9a6ccbb28
A-BO <86697442+lwb1978@users.noreply.github.com>
2026-07-03 13:44:45 +0800

    libcurl HTTP3

--- immortalwrt/diy-part2.sh
+++ immortalwrt/diy-part2.sh
@@ -150,16 +150,16 @@ sed -i 's/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/ut
 # git clone https://github.com/sbwml/package_libs_ngtcp2 feeds/packages/libs/ngtcp2
 
 # curl
-rm -rf feeds/packages/net/curl
-git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
+# rm -rf feeds/packages/net/curl
+# git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
 
 # 替换curl修改版（无nghttp3、ngtcp2）
-curl_ver=$(cat feeds/packages/net/curl/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}')
-[ "$(check_ver "$curl_ver" "8.12.0")" != "0" ] && {
-	echo "替换curl版本"
-	rm -rf feeds/packages/net/curl
-	cp -rf ${GITHUB_WORKSPACE}/patch/curl feeds/packages/net/curl
-}
+#curl_ver=$(cat feeds/packages/net/curl/Makefile | grep -i "PKG_VERSION:=" | awk 'BEGIN{FS="="};{print $2}')
+#[ "$(check_ver "$curl_ver" "8.12.0")" != "0" ] && {
+#	echo "替换curl版本"
+#	rm -rf feeds/packages/net/curl
+#	cp -rf ${GITHUB_WORKSPACE}/patch/curl feeds/packages/net/curl
+#}
 
 mirror=raw.githubusercontent.com/sbwml/r4s_build_script/master
 
